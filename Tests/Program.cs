5f4a4ebcb3aa9bc5993d4a32175500c5d8042167
Edlejo <jonas.edler@stud.hs-kempten.de>
2023-01-05 08:25:33 +0100

    this works

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine($"{"Hello, World!",-25} {DateTime.Now,-25:D}");
-
+            //Hier i will change somesthing
         }
     }
 }
